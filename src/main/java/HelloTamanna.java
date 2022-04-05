f91d4f47d7a9438b33680f2516ee4dfd96b18828
Tamanna Soni <tamanna.soni@publicissapient.com>
2022-04-05 18:01:11 +0530

    Uploading assesment

---
+++ src/main/java/HelloTamanna.java
@@ -0,0 +1,12 @@
+package org;
+
+public class HelloTamanna {
+
+	public static void main(String[] args) {
+		String firstName="Tamanna";
+		String lastName="Soni";
+		System.out.println("Hello  "+firstName+" "+lastName);
+
+	}
+
+}
